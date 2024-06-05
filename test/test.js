12f071f9638007374facfa878986089611a7ac55
Ori Carmely <130679187+carmelyori@users.noreply.github.com>
2024-06-05 17:43:53 +0300

    Update test script

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ const assert = require('assert');
 describe('Hello World Page', function() {
   it('should return Hello, World!', async function() {
     const response = await axios.get('http://localhost:3000');
-    assert(response.data includes('Hello, World!'));
+    assert(response.data.includes('Hello, World!'));
   });
 });
