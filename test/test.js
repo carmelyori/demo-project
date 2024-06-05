071e9b88e5bdc29ae71c4327828b137a15b899ce
Ori Carmely <130679187+carmelyori@users.noreply.github.com>
2024-06-05 17:28:10 +0300

    Add test script

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+const assert = require('assert');
+
+describe('Hello World Page', function() {
+  it('should return Hello, World!', async function() {
+    const response = await axios.get('http://localhost:3000');
+    assert(response.data includes('Hello, World!'));
+  });
+});
